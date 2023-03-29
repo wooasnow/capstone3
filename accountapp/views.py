0e3d563da06bba7f00ae2d6422b38b5c938a3d46
hms5960 <hms5960@naver.com>
2023-03-29 18:24:53 +0900

    django 21

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -17,6 +20,17 @@ def hello_world(request):
 
         hello_world_list = HelloWorld.objects.all()
         return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        # reverse는 함수에서
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+
+class AccountView(CreateView):
+    model = User
+    # 장고에서 제공하는 기본 모델
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    # reverse_lazy는 클래스에서
+    template_name = 'accountapp/create.html'
+    # 어느 html파일로 갈지
