d598b23f2afbb6c237620721338e2e9d89077a99
hms5960 <hms5960@naver.com>
2023-03-31 13:14:31 +0900

    django31

--- profileapp/models.py
+++ profileapp/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class Profile(models.Model):
     # on_delete는 유저객체가 사라지면, 그와 연결된 프로필객체가 사라지는 것
-    user = models.OneToOneFiled(User, on_delete=models.CASCADE, related_name='profile')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     # 프로필 사진, 이미지 파일 서버에 저장 /media/profile에 저장
     image = models.ImageField(upload_to='profile/', null=True)
     #닉네임 unique 하나만 유일하게
