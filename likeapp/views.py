f67703a14fa2d95de6a4fbdc6f30c8f153443a2a
hms5960 <hms5960@naver.com>
2023-04-09 15:00:03 +0900

    django add2

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
+from django.db import transaction
 from django.http import HttpResponseRedirect
+from django.core.exceptions import ValidationError
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.utils.decorators import method_decorator
@@ -7,6 +9,18 @@ from django.views.generic import RedirectView
 from django.contrib import messages
 from articleapp.models import Article
 from likeapp.models import LikeRecord
+
+
+@transaction.atomic
+def db_transaction(user, article):
+    article.like += 1
+    article.save()
+
+    if LikeRecord.objects.filter(user=user, article=article).exists():
+        raise ValidationError('Like already exists')
+    else:
+        LikeRecord(user=user, article=article).save()
+
 
 
 @method_decorator(login_required, 'get')
@@ -19,14 +33,12 @@ class LikeArticleView(RedirectView):
 
         user = self.request.user
         article = get_object_or_404(Article, pk=kwargs['pk'])
+        try:
 
-        if LikeRecord.objects.filter(user=user, article=article).exists():
-            messages.add_message(self.request, messages.ERROR, '좋아요는 한번만 가능합니다.')
-            return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk' : kwargs['pk']}))
-        else:
-            LikeRecord(user=user, article=article).save()
+            db_transaction(user, article)
+            messages.add_message(self.request, messages.SUCCESS, '좋아요 성공!')
 
-        article.like += 1
-        article.save()
-        messages.add_message(self.request, messages.SUCCESS, '좋아요 성공!')
+        except ValidationError:
+            messages.add_message(self.request, messages.ERROR, '이미 좋아요를 누르셨습니다.')
+            return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk': kwargs['pk']}))
         return super(LikeArticleView, self).get(self.request, *args, **kwargs)
